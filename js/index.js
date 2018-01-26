c7b628cc3c5610b161dbac91dd8062eef7b4a015
Lucas Braun <lucasalan@gmail.com>
2018-01-26 13:31:43 +0100

    Add functionality to make marker on click

--- js/index.js
+++ js/index.js
@@ -2,6 +2,11 @@ var app = angular.module("app", ["leaflet-directive"]);
 
 app.controller("IndexController", ["$scope", function($scope) {
   angular.extend($scope, {
+    center: {
+        lat: 39.98685368305097,
+        lng: -0.04566192626953125,
+        zoom: 15
+    },
     tiles: {
       url: "http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png",
       options: {
@@ -9,4 +14,16 @@ app.controller("IndexController", ["$scope", function($scope) {
       }
     }
   })
+
+  $scope.markers = new Array();
+
+  $scope.$on("leafletDirectiveMap.click", function (event,args) {
+    var latlng = args.leafletEvent.latlng;
+
+    $scope.markers = [{
+      lat: latlng.lat,
+      lng: latlng.lng,
+      message: "Big Material"
+    }]
+  });
 }]);
