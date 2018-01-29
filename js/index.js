ad3f8a31efdb951bdd4a5c7da3df30dbed9d9cc0
Lucas Braun <lucasalan@gmail.com>
2018-01-29 11:09:48 +0100

    Add to do list functionality

--- js/index.js
+++ js/index.js
@@ -26,8 +26,36 @@ app.controller("IndexController", ["$scope", function($scope) {
       $scope.markers.push({
         lat: latlng.lat,
         lng: latlng.lng,
-        message: "Hello"
+        message: "New entry",
+        dueDate: new Date()
       });
+
+      $scope.currentMarker = $scope.markers[$scope.markers.length - 1]
+      $("#modal").modal('show');
     }
   });
+
+  $scope.currentMarker = {};
+
+  $scope.showInfo = function(index) {
+    $scope.currentMarker = $scope.markers[index];
+  }
+
+  $scope.show = function(index) {
+    $.each($scope.markers, function(i, marker) {
+      marker.focus = i == index;
+    });
+
+    $scope.center = {
+      lat: $scope.markers[index].lat,
+      lng: $scope.markers[index].lng,
+      zoom: 15
+    };
+  }
+
+  $scope.remove = function(index) {
+    if (index !== -1) {
+      $scope.markers.splice(index, 1);
+    }
+  }
 }]);
